ed1768629d1095a0eb25698e504310ccbc41aed0
ivanov6444 <a.ivanov6444@gmail.com>
2022-10-12 10:03:15 +0300

    added components todo-add & todo-list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TodoAddComponent } from './todo-add/todo-add.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoAddComponent,
+    TodoListComponent
   ],
   imports: [
     BrowserModule
